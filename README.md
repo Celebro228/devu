682044e46fec30437e8aa0e59be4af2d30997bcd
Celebro228 <zzirioss@gmail.com>
2026-01-26 11:47:44 +0500

    non function web

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 DeVu - easy and fast 3d raylib-based game engine.
+
+
+1. Install Cmake (for Raylib-backend): https://cmake.org/download/
---
+++ web/src/main.rs
@@ -0,0 +1,39 @@
+use devu::prelude::*;
+
+
+fn main() {
+    devu::run("Camera2D");
+}
+
+
+#[startup]
+fn spawn(
+    mut commands: Commands,
+) {
+    commands.spawn((
+        Camera2D,
+        position(0, 0, 0),
+        rotation2d(0),
+    ));
+    commands.spawn((
+        rect(10000, 300),
+        RED,
+    ));
+    commands.spawn((
+        rect(300, 10000),
+        BLUE,
+    ));
+    commands.spawn((
+        rect(300, 300),
+        WHITE,
+    ));
+}
+
+
+#[update]
+fn update_camera(
+    mut camera_rotation: Single<&mut Rotation2D, With<Camera2D>>,
+    time: Res<Time>,
+) {
+    camera_rotation.0 += time.delta() * 90.;
+}
